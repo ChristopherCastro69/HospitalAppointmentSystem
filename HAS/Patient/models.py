e7e2d917eb4cd0d0650a16f5aedd6cf33375309b
VennDo1 <venndominic.delapena@cit.edu>
2023-10-06 01:01:32 +0800

    Patient/models.py

--- HAS/Patient/models.py
+++ HAS/Patient/models.py
@@ -3,4 +3,9 @@ from User.models import User
 
 # Create your models here.
 class Patient(User):
-    patientID = models.AutoField(primary_key=True)
+    #patientID = models.AutoField(primary_key=True)
+    pass
+
+class MedicalHistory(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    description = models.CharField(max_length=20)
